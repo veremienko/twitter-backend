815849d83ed560517125c56dc3e5ee7f92d8b364
veremiienko <andrii.veremiienko@ekreative.com>
2026-08-01 10:22:39 +0300

    Add migration for user profile fields

---
+++ services/auth-service/drizzle/0002_right_mysterio.sql
@@ -0,0 +1,3 @@
+ALTER TABLE "users" ADD COLUMN "name" text NOT NULL;--> statement-breakpoint
+ALTER TABLE "users" ADD COLUMN "age" integer NOT NULL;--> statement-breakpoint
+ALTER TABLE "users" ADD COLUMN "sex" text NOT NULL;
